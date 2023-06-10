824043bb33390413b9ad718772f2f6c47a5cbeef
Viktor.A <viktor.antonyshyn@gmail.com>
2023-06-10 16:37:55 +0200

    Added animation to black keys (audio no works)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Teclas blancas
 let keys = document.querySelectorAll('.white-key');
 
 keys.forEach(function(key) {
@@ -10,71 +11,27 @@ keys.forEach(function(key) {
       key.classList.remove('active-white');
     }, 300);
     
-    // Відтворення аудіофайлу
+    // Play audio
     audio.currentTime = 0;
     audio.play();
   });
 });
 
-/* 
-let keys = document.querySelectorAll('.white-key');
+// Teclas negras
+let keys2 = document.querySelectorAll('.black-key');
 
-keys.forEach(function(key) {
+keys2.forEach(function(key) {
   key.addEventListener('click', function() {
     let note = this.id;
     let audio = document.getElementById('audio-' + note);
     
-    // Зміна стану клавіші (додайте власні стилі для зміни вигляду)
-    this.classList.add('active-white');
+    this.classList.add('active-black');
     setTimeout(function() {
-      key.classList.remove('active-white');
+      key.classList.remove('active-black');
     }, 300);
     
-    // Відтворення аудіофайлу
+    // Play audio
     audio.currentTime = 0;
     audio.play();
   });
-}); 
-
-*/
-
-
-
-/*  Segunda manera
-let whiteKeys = document.querySelectorAll('.white-key');
-let blackKeys = document.querySelectorAll('.black-key');
-
-whiteKeys.forEach(function(key) {
-  key.addEventListener('click', function() {
-    playAudio(this.id);
-    animateKey(this);
-  });
 });
-
-blackKeys.forEach(function(key) {
-  key.addEventListener('click', function() {
-    playAudio(this.id);
-    animateKey(this);
-  });
-});
-
-function playAudio(keyId) {
-  // Відтворення аудіо-файлу відповідно до ноти
-  let audio = new Audio(keyId + '.mp3');
-  audio.play();
-}
-
-function animateKey(key) {
-  // Анімація клавіші при кліку
-  key.classList.add('active-white');
-  setTimeout(function() {
-    key.classList.remove('active-white');
-  }, 300);
-}
-
-*/
-
-
-
-
-
